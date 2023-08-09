2b9f4b577a122fae4babe5bf47327bfc0065b99e
Deepa Saini <113918047+deepa1922@users.noreply.github.com>
2023-08-09 20:34:09 +0530

    myAPI.py

--- myAPI.py
+++ myAPI.py
@@ -1,9 +1,9 @@
-#2zAP6eQuJZ0YtWdf1G34RY28avtUy0DlJzWN5FlDiJg
+
 import paralleldots
 
 class API:
     def __init__(self):
-        paralleldots.set_api_key('2zAP6eQuJZ0YtWdf1G34RY28avtUy0DlJzWN5FlDiJg')
+        paralleldots.set_api_key('API-KEY')
 
     def sentiment_analysis(self,text):
         response = paralleldots.sentiment(text)
